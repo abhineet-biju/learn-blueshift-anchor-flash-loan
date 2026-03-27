502091803ff1d8fb791f8ac0f34bbd1c6d87caff
Abhineet Biju <abhineet.biju19@gmail.com>
2026-03-27 20:14:59 +0530

    feat: wired up lib.rs

--- programs/blueshift_anchor_flash_loan/src/lib.rs
+++ programs/blueshift_anchor_flash_loan/src/lib.rs
@@ -1,13 +1,9 @@
-pub mod constants;
 pub mod error;
 pub mod instructions;
-pub mod state;
 
 use anchor_lang::prelude::*;
 
-pub use constants::*;
 pub use instructions::*;
-pub use state::*;
 
 declare_id!("6UrqmN5T95eyF51XfWcnHNoQq7HBHjLn8onRd8iiEa3d");
 
@@ -15,7 +11,10 @@ declare_id!("6UrqmN5T95eyF51XfWcnHNoQq7HBHjLn8onRd8iiEa3d");
 pub mod blueshift_anchor_flash_loan {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn borrow(ctx: Context<Borrow>, amount: u64) -> Result<()> {
+        borrow::handler(ctx, amount)
+    }
+    pub fn repay(ctx: Context<Repay>) -> Result<()> {
+        repay::handler(ctx)
     }
 }
