e14b042656df56159c31954187626b1c567bbc4d
Abhineet Biju <abhineet.biju19@gmail.com>
2026-03-27 18:50:42 +0530

    feat(borrow): add account context for borrow instruction

---
+++ programs/blueshift_anchor_flash_loan/src/instructions/borrow.rs
@@ -0,0 +1,44 @@
+use crate::error::*;
+use anchor_lang::prelude::*;
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token_interface::{
+    transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked,
+};
+
+#[derive(Accounts)]
+pub struct Borrow<'info> {
+    #[account(mut)]
+    pub borrower: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"protocol".as_ref()],
+        bump
+        )]
+    pub protocol: SystemAccount<'info>,
+
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        init_if_needed,
+        payer = borrower,
+        associated_token::mint = mint,
+        associated_token::authority = borrower,
+        associated_token::token_program = token_program
+        )]
+    pub borrower_ata: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = protocol
+        )]
+    pub protocol_ata: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(address = INSTRUCTIONS_SYSVAR_ID)]
+    instructions: UncheckedAccount<'info>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
